2383de3510deec1624636f0d56045bcfbc1bdae6
rainwalker <kinghu1990@sina.com>
2019-06-20 17:29:05 +0800

    feat: hack slot-scope

--- src/views/table-render.vue
+++ src/views/table-render.vue
@@ -1,10 +1,40 @@
 <template>
   <div>
-    <table-render :columns="columns" :data="data"></table-render>
+    <table-render ref="table" :columns="columns" :data="data">
+      <template slot-scope="{ row, index }" slot="name">
+        <input type="text" v-model="editName" v-if="editIndex === index" />
+        <span v-else>{{ row.name }}</span>
+      </template>
+
+      <template slot-scope="{ row, index }" slot="age">
+        <input type="text" v-model="editAge" v-if="editIndex === index" />
+        <span v-else>{{ row.age }}</span>
+      </template>
+
+      <template slot-scope="{ row, index }" slot="birthday">
+        <input type="text" v-model="editBirthday" v-if="editIndex === index" />
+        <span v-else>{{ getBirthday(row.birthday) }}</span>
+      </template>
+
+      <template slot-scope="{ row, index }" slot="address">
+        <input type="text" v-model="editAddress" v-if="editIndex === index" />
+        <span v-else>{{ row.address }}</span>
+      </template>
+
+      <template slot-scope="{ row, index }" slot="action">
+        <div v-if="editIndex === index">
+          <button @click="handleSave(index)">保存</button>
+          <button @click="editIndex = -1">取消</button>
+        </div>
+        <div v-else>
+          <button @click="handleEdit(row, index)">操作</button>
+        </div>
+      </template>
+    </table-render>
   </div>
 </template>
 <script>
-  import TableRender from '@/components/table-render/table.vue';
+  import TableRender from '../components/table-render/table.vue';
 
   export default {
     components: { TableRender },
@@ -13,128 +43,130 @@
         columns: [
           {
             title: '姓名',
-            key: 'name',
-            render: (h, { row, index }) => {
-              let edit;
-
-              // 当前行为聚焦行时
-              if (this.editIndex === index) {
-                edit = [h('input', {
-                  domProps: {
-                    value: row.name
-                  },
-                  on: {
-                    input: (event) => {
-                      this.editName = event.target.value;
-                    }
-                  }
-                })];
-              } else {
-                edit = row.name;
-              }
-              return h('div', [
-                edit
-              ]);
+            render: (h, { row, column, index }) => {
+              return h(
+                'div',
+                this.$refs.table.$scopedSlots.name({
+                  row: row,
+                  column: column,
+                  index: index
+                })
+              )
             }
           },
           {
             title: '年龄',
-            key: 'age'
+            render: (h, { row, column, index }) => {
+              return h(
+                'div',
+                this.$refs.table.$scopedSlots.age({
+                  row: row,
+                  column: column,
+                  index: index
+                })
+              )
+            }
           },
           {
             title: '出生日期',
-            key: 'birthday',
             render: (h, { row, column, index }) => {
-              const date = new Date(parseInt(row.birthday));
-              const year = date.getFullYear();
-              const month = date.getMonth() + 1;
-              const day = date.getDate();
-              
-              const birthday = `${year}-${month}-${day}`;
-              
-              return h('span', birthday);
+              console.log(this.$refs.table)
+              return h(
+                'div',
+                this.$refs.table.$scopedSlots.birthday({
+                  row: row,
+                  column: column,
+                  index: index
+                })
+              )
             }
           },
           {
             title: '地址',
-            key: 'address'
+            render: (h, { row, column, index }) => {
+              return h(
+                'div',
+                this.$refs.table.$scopedSlots.address({
+                  row: row,
+                  column: column,
+                  index: index
+                })
+              )
+            }
           },
           {
             title: '操作',
-            render: (h, { row, index }) => {
-              // 如果当前行是编辑状态，则渲染两个按钮
-              if (this.editIndex === index) {
-                return [
-                  h('button', {
-                    on: {
-                      click: () => {
-                        this.data[index].name = this.editName;
-                        this.data[index].age = this.editAge;
-                        this.data[index].birthday = this.editBirthday;
-                        this.data[index].address = this.editAddress;
-                        this.editIndex = -1;
-                      }
-                    }
-                  }, '保存'),
-                  h('button', {
-                    style: {
-                      marginLeft: '6px'
-                    },
-                    on: {
-                      click: () => {
-                        this.editIndex = -1;
-                      }
-                    }
-                  }, '取消')
-                ];
-              } else {  // 当前行是默认状态，渲染为一个按钮
-                return h('button', {
-                  on: {
-                    click: () => {
-                      this.editName = row.name;
-                      this.editAge = row.age;
-                      this.editAddress = row.address;
-                      this.editBirthday = row.birthday;
-                      this.editIndex = index;
-                    }
-                  }
-                }, '修改');
-              }
+            render: (h, { row, column, index }) => {
+              return h(
+                'div',
+                this.$refs.table.$scopedSlots.action({
+                  row: row,
+                  column: column,
+                  index: index
+                })
+              )
             }
           }
         ],
-        data: [
-          {
-            name: '王小明',
-            age: 18,
-            birthday: '919526400000',
-            address: '北京市朝阳区芍药居'
-          },
-          {
-            name: '张小刚',
-            age: 25,
-            birthday: '696096000000',
-            address: '北京市海淀区西二旗'
-          },
-          {
-            name: '李小红',
-            age: 30,
-            birthday: '563472000000',
-            address: '上海市浦东新区世纪大道'
-          },
-          {
-            name: '周小伟',
-            age: 26,
-            birthday: '687024000000',
-            address: '深圳市南山区深南大道'
-          }
-        ],
-        editName: '',  // 第一列输入框
+        data: [],
+        editIndex: -1,  // 当前聚焦的输入框的行数
+        editName: '',  // 第一列输入框，当然聚焦的输入框的输入内容，与 data 分离避免重构的闪烁
         editAge: '',  // 第二列输入框
         editBirthday: '',  // 第三列输入框
         editAddress: '',  // 第四列输入框
-         editIndex: -1  // 当前聚焦的输入框的行数
       }
+    },
+    methods: {
+      handleEdit (row, index) {
+        this.editName = row.name;
+        this.editAge = row.age;
+        this.editAddress = row.address;
+        this.editBirthday = row.birthday;
+        this.editIndex = index;
+      },
+      handleSave (index) {
+        this.data[index].name = this.editName;
+        this.data[index].age = this.editAge;
+        this.data[index].birthday = this.editBirthday;
+        this.data[index].address = this.editAddress;
+        this.editIndex = -1;
+      },
+      getBirthday (birthday) {
+        const date = new Date(parseInt(birthday));
+        const year = date.getFullYear();
+        const month = date.getMonth() + 1;
+        const day = date.getDate();
+
+        return `${year}-${month}-${day}`;
+      }
+    },
+    mounted () {
+      this.data = [
+        {
+          name: '王小明',
+          age: 18,
+          birthday: '919526400000',
+          address: '北京市朝阳区芍药居'
+        },
+        {
+          name: '张小刚',
+          age: 25,
+          birthday: '696096000000',
+          address: '北京市海淀区西二旗'
+        },
+        {
+          name: '李小红',
+          age: 30,
+          birthday: '563472000000',
+          address: '上海市浦东新区世纪大道'
+        },
+        {
+          name: '周小伟',
+          age: 26,
+          birthday: '687024000000',
+          address: '深圳市南山区深南大道'
+        }
+      ];
     }
   }
 </script>
