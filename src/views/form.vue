b20bcb083c07a38531ec3d0d3da8425584d1cb4b
rainwalker <kinghu1990@sina.com>
2019-07-04 11:30:27 +0800

    feat(section1): 添加重置表单方法及其示例

--- src/views/form.vue
+++ src/views/form.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="form">
     <h1 style="color: rgb(0, 127, 255)">具有校验功能的表单</h1>
-    <i-form :model="formValidate" :rules="ruleValidate">
+    <i-form :model="formValidate" :rules="ruleValidate" ref="form">
       <i-form-item label="用户名" prop="name">
         <i-input v-model="formValidate.name"></i-input>
       </i-form-item>
@@ -9,6 +9,8 @@
         <i-input v-model="formValidate.mail"></i-input>
       </i-form-item>
     </i-form>
+    <button @click="handleSubmit">提交</button>
+    <button @click="handleReset">重置</button>
   </div>
 </template>
 <script>
@@ -32,6 +34,20 @@ export default {
         ]
       }
     };
+  },
+  methods: {
+    handleSubmit() {
+      this.$refs.form.validate(valid => {
+        if (valid) {
+          window.alert("提交成功");
+        } else {
+          window.alert("表单校验失败");
+        }
+      });
+    },
+    handleReset() {
+      this.$refs.form.resetFields();
+    }
   }
 };
 </script>
