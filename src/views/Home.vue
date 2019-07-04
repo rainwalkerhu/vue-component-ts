36300a71a6defae73615174ddfc5c97424056866
rainwalker <kinghu1990@sina.com>
2019-07-04 10:06:19 +0800

    feat(section1):init form page feat(section1):init form page feat(section1):init form page

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,46 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <h1 style="color: rgb(0, 127, 255)">Vue.js 组件精讲</h1>
+    <el-row :gutter="16">
+      <el-col :span="12" :offset="6">
+        <el-table :data="tableData">
+          <el-table-column prop="title" label="课程名称"></el-table-column>
+          <el-table-column prop="date" label="时间"></el-table-column>
+          <el-table-column label="操作" width="100">
+            <template slot-scope="scope">
+              <el-button
+                @click="handleClick(scope.row)"
+                type="text"
+                size="small"
+                >查看</el-button
+              >
+            </template>
+          </el-table-column>
+        </el-table>
+      </el-col>
+    </el-row>
   </div>
 </template>
-
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
-  name: "home",
-  components: {
-    HelloWorld
+  methods: {
+    handleClick(row) {
+      this.$router.push({
+        name: row.name
+      });
+    }
+  },
+
+  data() {
+    return {
+      tableData: [
+        {
+          date: "2019-07-04",
+          title: "具有校验功能的表单组件",
+          name: "form"
+        }
+      ]
+    };
   }
 };
 </script>
---
+++ src/views/form.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="form">
+    <h1 style="color: rgb(0, 127, 255)">具有校验功能的表单</h1>
+  </div>
+</template>
+<script>
+export default {
+  methods: {},
+
+  data() {
+    return {};
+  }
+};
+</script>
